f7e9dd0c2bd9d544b167755a3213a6ae0dd4b31e
Wisteria30 <skymars0327@yahoo.co.jp>
2018-08-07 20:19:58 +0900

    React-router追加

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import TimeTablePage from "./TimeTable/TimeTablePage"
+import TimeTablePage from './TimeTable/TimeTablePage';
 
 class App extends Component {
   render() {
