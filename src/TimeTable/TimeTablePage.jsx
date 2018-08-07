eb3e2c0a89b62445af78fcf2bd4868a8e2175a54
Wisteria30 <skymars0327@yahoo.co.jp>
2018-08-08 03:42:58 +0900

    一応最低限の形だけは完成？ あとはデザインで見栄えを整えたらとりあえずはOK？ （まだまだ修正点あり）

--- src/TimeTable/TimeTablePage.jsx
+++ src/TimeTable/TimeTablePage.jsx
@@ -3,71 +3,87 @@ import Timeline from 'react-calendar-timeline';
 import 'react-calendar-timeline/lib/Timeline.css';
 import moment from 'moment';
 
-const groups = [{id: 1, title: 'group 1'}, {id: 2, title: 'group 2'}];
-
-const items = [
-  {
-    id: 1,
-    group: 1,
-    title: 'item 1',
-    start_time: moment(),
-    end_time: moment().add(1, 'hour'),
-  },
-  {
-    id: 2,
-    group: 2,
-    title: 'item 2',
-    start_time: moment().add(-0.5, 'hour'),
-    end_time: moment().add(0.5, 'hour'),
-  },
-  {
-    id: 3,
-    group: 1,
-    title: 'item 3',
-    start_time: moment().add(2, 'hour'),
-    end_time: moment().add(3, 'hour'),
-  },
-];
-
 export default class TimeTablePage extends Component {
   constructor(state) {
     super(state);
     this.state = {
-      groups: [{id: 1, title: 'group 1'}, {id: 2, title: 'group 2'}],
-      items: [
-        {
-          id: 1,
-          group: 1,
-          title: 'item 1',
-          start_time: moment(),
-          end_time: moment().add(1, 'hour'),
-        },
-        {
-          id: 2,
-          group: 2,
-          title: 'item 2',
-          start_time: moment().add(-0.5, 'hour'),
-          end_time: moment().add(0.5, 'hour'),
-        },
-        {
-          id: 3,
-          group: 1,
-          title: 'item 3',
-          start_time: moment().add(2, 'hour'),
-          end_time: moment().add(3, 'hour'),
-        },
-      ],
+      groups: [],
+      items: [],
       who: '',
       what: '',
-      begin: null,
-      end: null,
+      startDate: null,
+      startTime: null,
+      endDate: null,
+      endTime: null,
     };
     this.onChangeField = this.onChangeField.bind(this);
+    this.createTable = this.createTable.bind(this);
+    this.clearTable = this.clearTable.bind(this);
   }
 
   // stateの更新
   onChangeField(e, key) {
     this.setState({[key]: e.target.value});
+  }
+
+  createTable() {
+    // タイムラインバーの追加
+    const changeGroup = [
+      {id: this.state.groups.length + 1, title: this.state.who},
+    ];
+    let newGroups;
+    if (
+      this.state.groups.find(name => {
+        return name.title === this.state.who;
+      }) === undefined
+    ) {
+      newGroups = this.state.groups.concat(changeGroup);
+    } else {
+      newGroups = this.state.groups;
+    }
+    this.setState({groups: newGroups});
+
+    // タイムラインの内容追加
+    const grNum =
+      this.state.groups.findIndex((value, index, sourceArray) => {
+        return value.title === this.state.who;
+      }) + 1;
+    const changeItem = [
+      {
+        id: this.state.items.length + 1,
+        group: grNum,
+        title: this.state.what,
+        start_time: moment(
+          this.state.startDate + ' ' + this.state.startTime,
+          'YYYY-MM-DD HH:mm'
+        ),
+        end_time: moment(
+          this.state.endDate + ' ' + this.state.endTime,
+          'YYYY-MM-DD HH:mm'
+        ),
+      },
+    ];
+    let newItems;
+    if (this.state.what !== '') {
+      newItems = this.state.items.concat(changeItem);
+    } else {
+      newItems = this.state.items;
+    }
+    this.setState({items: newItems});
+    console.log(grNum);
+    console.log(
+      moment(
+        this.state.startDate + ' ' + this.state.startTime,
+        'YYYY-MM-DD HH:mm'
+      )
+    );
+    console.log(
+      moment(this.state.endDate + ' ' + this.state.endTime, 'YYYY-MM-DD HH:mm')
+    );
+  }
+
+  clearTable() {
+    this.setState({groups: []});
   }
 
   render() {
@@ -91,14 +107,25 @@ export default class TimeTablePage extends Component {
         </div>
         <div className="When_begin">
           <p>When begin ?</p>
-          <input type="time" onChange={e => this.onChangeField(e, 'begin')} />
+          <input
+            type="date"
+            onChange={e => this.onChangeField(e, 'startDate')}
+          />
+          <input
+            type="time"
+            onChange={e => this.onChangeField(e, 'startTime')}
+          />
         </div>
         <div className="When_end">
           <p>When end ?</p>
-          <input type="time" onChange={e => this.onChangeField(e, 'end')} />
+          <input type="date" onChange={e => this.onChangeField(e, 'endDate')} />
+          <input type="time" onChange={e => this.onChangeField(e, 'endTime')} />
         </div>
         <div className="push_line">
           <button onClick={this.createTable}>Enter</button>
+        </div>
+        <div className="clear">
+          <button onClick={this.clearTable}>Clear</button>
         </div>
 
         <Timeline
